e48a43e18bb06515edec8ae03f17cd8b7bf440eb
suroh <roh7891backup@gmail.com>
2024-11-12 19:24:11 +0000

    updated

--- RANK1/libft/ft_strlen.c
+++ RANK1/libft/ft_strlen.c
@@ -6,13 +6,12 @@
 /*   By: suroh <suroh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 12:59:21 by suroh             #+#    #+#             */
-/*   Updated: 2024/04/24 12:09:19 by suroh            ###   ########.fr       */
+/*   Updated: 2024/11/12 19:23:06 by suroh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-size_t	ft_strlen(const char *s)
+/*size_t	ft_strlen(const char *s)
 {
 	size_t	i;
 
@@ -21,7 +20,18 @@ size_t	ft_strlen(const char *s)
 		i++;
 	return (i);
 }
+*/
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
 
+	i = 0;
+	if (!str)
+		return (i);
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
 // int	main()
 // {
 // 	printf("%zu\n", ft_strlen("this"));
